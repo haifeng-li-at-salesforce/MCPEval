3c6504a4a286ba894b09d9fcd9e1b32ab23fb244
haifeng.li <haifeng.li@salesforce.com>
2025-10-24 15:31:08 -0700

    fix: downgrade zod version

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { testStreamingClient } from './clients/streaming-client';
 import { testAiClient } from './utils/ai-client';
 import { LLMExpressModel, EinsteinDevModel, QWEN_CONFIG } from './model/model-configs';
 import { testLLMExpressModelClient } from './clients/chatcompletion-client';
+import { toolDiscoveryWorkflow } from './eval/tool-discovery-workflow';
 
 // Initialize the OpenAI client
 
 async function main() {
   //testStreamingClient(QWEN_CONFIG);
-  testAiClient();
+  //testAiClient();
+  await toolDiscoveryWorkflow();
   //testLLMExpressModelClient(LLMExpressModel.GEMINI_2_5_FLASH);
 }
 
